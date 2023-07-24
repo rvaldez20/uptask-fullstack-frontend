8950fa40d569b97fe66cfe219cc7ec3a374ef465
centinela1024tb <centinela1024tb@gmail.com>
2023-07-24 14:41:44 -0600

    [ADD] maquetación del login
    => input para email y password
    => input tipo submit para iniciar sesión

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,46 @@
 
 const Login = () => {
    return (
-      <div>Login</div>
+      <>
+         <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesión y administra tus {' '}
+            <span className="text-slate-700">proyectos</span>
+         </h1>
+
+         <form className="my-10 bg-white shadow rounded-lg p-10">
+            <div className="my-5">
+               <label 
+                  className="uppercase text-gray-600 block text-xl font-bold"                   
+                  htmlFor="email"
+               >Email</label>
+               <input 
+                  className="w-full mt-3 p-3 border rounded-lg bg-gray-50"
+                  type="email"
+                  placeholder="Email de Registro"
+                  id="email"
+               />
+            </div>
+
+            <div className="my-5">
+               <label 
+                  className="uppercase text-gray-600 block text-xl font-bold"                   
+                  htmlFor="password"
+               >Password</label>
+               <input 
+                  className="w-full mt-3 p-3 border rounded-lg bg-gray-50"
+                  type="password"
+                  placeholder="Password de Registro"
+                  id="Password"
+               />
+            </div>
+
+            <input 
+               type="submit"
+               value="Iniciar Sesión"
+               className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+            />
+         </form>
+
+      </>
    )
 }
 
