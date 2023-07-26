2f77c3e88cbfb8c31da5b1c75a827fa44983989e
centinela1024tb <centinela1024tb@gmail.com>
2023-07-26 14:47:01 -0600

    [ADD] se implementa el context: AuthContext y AuthProvider y se usa en App.jsx envolviendo toda la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,27 @@ import OlvidePassword from './pages/OlvidePassword'
 import NuevoPassword from './pages/NuevoPassword'
 import ConfirmarCuenta from './pages/ConfirmarCuenta'
 
+import { AuthProvider } from './context/AuthProvider'
+
 // console.log(import.meta.env.VITE_BACKEND_URL) // acceso a las variables de entorno en VITE
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
+      <AuthProvider>
+        <Routes>
 
-        <Route path="/" element={<AuthLayout />} >
-          <Route index element={<Login />} />
-          <Route path="registrar" element={<Registrar />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword />} />
-          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />    
-        </Route>      
-          
-      </Routes>
+          <Route path="/" element={<AuthLayout />} >
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />    
+          </Route>      
+            
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
