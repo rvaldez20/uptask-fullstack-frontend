81cec185a387af8a581c41e0bf906f9eea84d5e3
centinela1024tb <centinela1024tb@gmail.com>
2023-07-26 12:51:54 -0600

    [ADD] se impelemnta logica del enlace que ese envia por correo al solicitar olvide password
    => se valida el token haciendo un request a la API
    => si token valido se muestra el formulario para ingresar la nueva contraseña
    => si no es token invalido se muestra alerta

--- src/pages/NuevoPassword.jsx
+++ src/pages/NuevoPassword.jsx
@@ -1,33 +1,66 @@
+import { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+import Alerta from '../components/Alerta'
 
 
 const NuevoPassword = () => {
+
+   //states
+   const [alerta, setAlerta] = useState({})
+   const [tokenValido, setTokenValido] = useState(false)
+
+   const params = useParams();
+   const {token} = params
+
+   useEffect(() => {
+      const comprobarToken = async () => {
+         try {            
+            await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios/olvide-password/${token}`)
+            setTokenValido(true)
+
+         } catch (error) {
+            setAlerta({
+               msg: error.response.data.msg,
+               error: true
+            })
+         }
+      }
+      comprobarToken()
+   }, [])
+
+   const { msg } = alerta;
+
    return (
       <>
          <h1 className="text-sky-600 font-black text-5xl capitalize">Restablece tu Password y no Pierdas Acceso a tus {' '}
             <span className="text-slate-700">Proyectos</span>
          </h1>
 
-         <form className="my-10 bg-white shadow rounded-lg p-10">            
-            
-            <div className="my-5">
-               <label 
-                  className="uppercase text-gray-600 block text-xl font-bold"                   
-                  htmlFor="password"
-               >Nuevo Password</label>
-               <input 
-                  className="w-full mt-3 p-3 border rounded-lg bg-gray-50"
-                  type="password"
-                  placeholder="Escribe tu Nuevo Password"
-                  id="password"
-               />
-            </div>          
+         { msg && <Alerta alerta={alerta} />}
 
-            <input 
-               type="submit"
-               value="Guardar Nuevo Password"
-               className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-            />
-         </form>
+         { tokenValido && (
+            <form className="my-10 bg-white shadow rounded-lg p-10">                        
+               <div className="my-5">
+                  <label 
+                     className="uppercase text-gray-600 block text-xl font-bold"                   
+                     htmlFor="password"
+                  >Nuevo Password</label>
+                  <input 
+                     className="w-full mt-3 p-3 border rounded-lg bg-gray-50"
+                     type="password"
+                     placeholder="Escribe tu Nuevo Password"
+                     id="password"
+                  />
+               </div>          
+
+               <input 
+                  type="submit"
+                  value="Guardar Nuevo Password"
+                  className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+               />
+            </form>   
+         )}
         
       </>
    )
