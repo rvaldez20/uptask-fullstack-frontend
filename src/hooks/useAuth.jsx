2dd01eeefed2cc8870069708b547f5672c3bc269
centinela1024tb <centinela1024tb@gmail.com>
2023-07-26 15:41:55 -0600

    [ADD] se implementa un hook para e context: useAuth

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import AuthProvider from '../context/AuthProvider'
+
+const useAuth = () => {
+  
+   return useContext(AuthProvider)
+}
+
+export default useAuth;
