3ad8d1ec5a94171a4b9d36085a2196e7179e7158
centinela1024tb <centinela1024tb@gmail.com>
2023-07-26 15:42:36 -0600

    [ADD] se implementa la logica para la authenticacion, verificando el token y haciendo la peticion enviando los headers

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,50 @@
 import { useState, useEffect, createContext } from 'react'
+import clienteAxios from '../config/clienteAxios';
+
 
 const AuthContext = createContext();
 
-const AuthProvider  = ({children}) => {
+const AuthProvider = ({children}) => {
 
+   const [auth, setAuth] = useState({});
+
+   useEffect(() => {      
+      const autenticarUsuario = async () => {
+         const token = localStorage.getItem('token')
+         
+         // verificamos si tenemos un token en localstorage
+         if(!token) {
+            return;
+         }
+
+         const configAuth = {
+            headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${token}`
+            }
+         }
+
+         try {
+            // hacemos el request a la API end-point perfil y obtenemos la info del usuario
+            const { data } = await clienteAxios.get('/usuarios/perfil', configAuth)
+
+            // colocamos la respuesta en el state auth
+            setAuth(data)
+
+         } catch (error) {
+            console.log(error)
+         }
+         
+      }
+      autenticarUsuario();
+   }, [])
+   
 
    return (
       <AuthContext.Provider 
          value={{
-            
+            setAuth
+                        
          }}
       >
          {children}
