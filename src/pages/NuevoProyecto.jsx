59732b13cec516ed20ca011f2f22b1f3cef829a6
centinela1024tb <centinela1024tb@gmail.com>
2023-07-27 15:08:28 -0600

    [ADD] se hace la maquetacioón del formulario para crear un proyecto

--- src/pages/NuevoProyecto.jsx
+++ src/pages/NuevoProyecto.jsx
@@ -1,12 +1,12 @@
-
+import FormularioProyecto from "../components/FormularioProyecto"
 
 const NuevoProyecto = () => {
    return (
       <>
-         <h1 className="text-4xl font-black">Nuevo Proyecto</h1>
+         <h1 className="text-4xl font-black">Crear Proyecto</h1>
 
-         <div>
-            
+         <div className="mt-10 flex justify-center">
+            <FormularioProyecto />
          </div>
       </>
    )
