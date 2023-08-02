b85c2f3f3eaf32d9551bd2a86a01b79223d2067e
centinela1024tb <centinela1024tb@gmail.com>
2023-08-01 18:02:34 -0600

    [ADD] se empiezan a moestrar las tareas de cada proyecto

--- src/pages/Proyecto.jsx
+++ src/pages/Proyecto.jsx
@@ -3,6 +3,7 @@ import { useParams, Link } from 'react-router-dom'
 
 import useProyectos from '../hooks/useProyectos';
 import ModalFormularioTarea from '../components/ModalFormularioTarea';
+import Tarea from '../components/Tarea';
 
 const Proyecto = () => {
    //states
@@ -50,6 +51,19 @@ const Proyecto = () => {
             </svg>
             Nueva Tarea
          </button>
+
+         <p className='font-bold text-xl mt-10'>Tareas del Proyecto</p>
+
+         <div className='bg-white shadow mt-10 rounded-lg'>
+            {proyecto.tareas?.length 
+               ? proyecto.tareas?.map( tarea => (
+                     <Tarea 
+                        key={tarea._id} 
+                        tarea={tarea}
+                     />
+                  ))                 
+               : <p className='text-center text-xl my-10 p-10'>No hay tareas en este Proyecto</p>}
+         </div>
 
          <ModalFormularioTarea />
       </>       
