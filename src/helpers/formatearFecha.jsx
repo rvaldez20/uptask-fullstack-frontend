6037f8e6c3ce14dd4cb7eebbcf680ec150cb3d16
centinela1024tb <centinela1024tb@gmail.com>
2023-08-01 18:15:09 -0600

    [FIX] se ajusta el formateo de fecha para que sea la fecha exacta (no muestre la del dia anterior)

--- src/helpers/formatearFecha.jsx
+++ src/helpers/formatearFecha.jsx
@@ -1,9 +1,11 @@
 
 export const formatearFecha = fecha => {
+   console.log()
+   // const fecha1 = "2023-09-10"   
+   // const fecha2 = "09-10-2023"
 
-   console.log(fecha)
-
-   const nuevaFecha = new Date(fecha)
+   // para que tome la fecha correcta exacta
+   const nuevaFecha = new Date(fecha.split('T')[0].split('-'))
    const opciones = {
       weekday: 'long',
       year: 'numeric',
