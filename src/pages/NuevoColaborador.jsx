09b29102759ef25838afb3d5e8930adb4f459cc3
centinela1024tb <centinela1024tb@gmail.com>
2023-08-03 16:35:44 -0600

    [ADD] se implementa la page(NuevoCalaborador) con el formulario(FormularioColaborador) que realiza una busqueda por email

--- src/pages/NuevoColaborador.jsx
+++ src/pages/NuevoColaborador.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import FormularioColaborador from "../components/FormularioColaborador"
+import useProyectos from "../hooks/useProyectos"
+
 
 const NuevoColaborador = () => {
+   // context 
+   const { obtenerProyecto, proyecto, cargando } = useProyectos();
+
+   const params = useParams();
+   console.log(params)
+
+   // Effect
+   useEffect(() => {
+      obtenerProyecto(params.id)
+   }, [])
+
+   if(cargando) return 'Cargando...'
+
    return (
-      <div>NuevoColaborador</div>
+      <>
+         <h1 className="text-4xl font-black">Añadir Colaborador(a) al Proyecto: <span className="text-sky-700">{proyecto.nombre}</span> </h1>
+
+         <div className="mt-10 flex justify-center">
+            <FormularioColaborador />
+         </div>
+      </>
    )
 }
 
