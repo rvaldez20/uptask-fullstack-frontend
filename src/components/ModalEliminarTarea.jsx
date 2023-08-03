010b9d106b01f4921c96f4a42d5ce560a46b9418
centinela1024tb <centinela1024tb@gmail.com>
2023-08-03 13:54:22 -0600

    [ADD] de agrega en el boton Eliminar la función para eliminar la tarea (se pasa del provider)

--- src/components/ModalEliminarTarea.jsx
+++ src/components/ModalEliminarTarea.jsx
@@ -7,7 +7,7 @@ import useProyectos from '../hooks/useProyectos'
 
 const ModalEliminarTarea = () => {
 
-   const { modalEliminarTarea, handleModalEliminarTarea } = useProyectos()
+   const { modalEliminarTarea, handleModalEliminarTarea, eliminarTarea } = useProyectos()
  
    return (
       <Transition.Root show={ modalEliminarTarea } as={Fragment}>
@@ -83,6 +83,7 @@ const ModalEliminarTarea = () => {
                               <button
                                  type="button"
                                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                 onClick={ eliminarTarea }
                               >
                                  Eliminar
                               </button>
