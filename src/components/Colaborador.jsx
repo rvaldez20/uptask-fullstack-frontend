39cf9e070b11c3640cb69bb20d160c493b6f12fe
centinela1024tb <centinela1024tb@gmail.com>
2023-08-08 15:18:21 -0600

    [ADD] se aplican estilos al listado de colaboradores y se agrega boton de eliminar

--- src/components/Colaborador.jsx
+++ src/components/Colaborador.jsx
@@ -1,8 +1,22 @@
 
 const Colaborador = ({colaborador}) => {
-  return (
-    <div>{colaborador.nombre}</div>
-  )
+   const {nombre, email} = colaborador;
+
+   return (
+      <div className="border-b p-5 flex justify-between items-center">
+         <div>
+            <p className="mb-1 text-xl">{nombre}</p>
+            <p className="mb-1 text-sm text-gray-700">{email}</p>
+         </div>
+
+         <div>
+            <button
+               className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"               
+            >Eliminar</button>
+         </div>
+
+      </div>
+   )
 }
 
 export default Colaborador
