ac341d9acdb74be8082d7e81ea6fe0dd24901420
centinela1024tb <centinela1024tb@gmail.com>
2023-08-11 16:17:50 -0600

    [ADD] se muestra una alerta en el listado solo si es colaborador

--- src/components/PreviewProyecto.jsx
+++ src/components/PreviewProyecto.jsx
@@ -1,20 +1,28 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 const PreviewProyecto = ({proyecto}) => {
 
-   const {nombre, _id, cliente } = proyecto
+   const { auth} = useAuth()
+   const { nombre, _id, cliente, creador } = proyecto
 
    return (
 
-      <div className="border-b p-5 flex">
+      <div className="border-b p-5 flex justify-between">
 
-         <p className="flex-1">
-            {nombre}
+         <div className="flex items-center gap-2">
+            <p className="flex-1">
+               {nombre}
 
-            <span className="text-sm text-gray-500 uppercase">
-               {''} {cliente}
-            </span>
-         </p>
+               <span className="text-sm text-gray-500 uppercase">
+                  {''} {cliente}
+               </span>
+            </p>
+
+            { auth._id !== creador && (         
+               <p className="py-1 px-2 text-xs rounded-lg text-white bg-green-500 font-bold uppercase">Colaborador</p>
+            )}
+         </div>
 
          <Link
             to={`${_id}`}
