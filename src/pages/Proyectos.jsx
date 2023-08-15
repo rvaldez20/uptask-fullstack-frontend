66763fd4b12ba1338a9b5056746274dff4365203
centinela1024tb <centinela1024tb@gmail.com>
2023-08-15 15:14:33 -0600

    [ADD] se limpian los eventos .on y .emit

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -2,28 +2,12 @@ import { useEffect } from 'react'
 import PreviewProyecto from '../components/PreviewProyecto';
 import useProyectos from '../hooks/useProyectos'
 import Alerta from '../components/Alerta';
-import io from 'socket.io-client'
 
-let socket;
 
 const Proyectos = () => {
 
    const { proyectos, alerta } = useProyectos();   
-
-   useEffect(() => {      
-      socket = io(import.meta.env.VITE_BACKEND_URL);
-
-      // emitiendo un evento
-      socket.emit('prueba', proyectos)
-
-      // recibiendo un evento del server
-      socket.on('respuesta', (persona) => {
-         console.log('Desde el frontend')
-         console.log(persona)
-      })
-
-   })
-   
+ 
 
    const {msg} = alerta
 
